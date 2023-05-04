dc01aeb5043a03c3e438fcaaa681a85f1bbd804c
Peter Friese <peter@peterfriese.de>
2023-05-04 16:12:58 +0200

    ✨ Add user agent
    Signed-off-by: Peter Friese <peter@peterfriese.de>

--- Sources/GoogleGenerativeAI/GenerativeLanguage.swift
+++ Sources/GoogleGenerativeAI/GenerativeLanguage.swift
@@ -17,16 +17,18 @@ import Get
 
 /// The API client for the PaLM API.
 public class GenerativeLanguage {
+  
   private(set) var apiKey: String
 
   private lazy var apiClient: APIClient = {
     let baseURL = URL(string: "https://generativelanguage.googleapis.com")!
     return APIClient(baseURL: baseURL) { configuration in
       configuration.delegate = self
+      configuration.sessionConfiguration.httpAdditionalHeaders = ["x-goog-api-client": "genai-swift/0.1.0"]
     }
   }()
 
-  var session = URLSession.shared
+  private var session = URLSession.shared
 
   /// Initializes the PalM API client.
   ///
