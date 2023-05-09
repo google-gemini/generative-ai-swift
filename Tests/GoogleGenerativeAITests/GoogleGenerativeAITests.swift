e02d1b4cfd640247bedf205c9873b9ae419c8fa5
Peter Friese <peter@peterfriese.de>
2023-05-10 00:49:11 +0200

    Update indentation

--- Tests/GoogleGenerativeAITests/GoogleGenerativeAITests.swift
+++ Tests/GoogleGenerativeAITests/GoogleGenerativeAITests.swift
@@ -16,35 +16,35 @@ import XCTest
 @testable import GoogleGenerativeAI
 
 final class GenerativeLanguageTests: XCTestCase {
-    let apiKey = "<INSERT-YOUR-API-KEY>"
+  let apiKey = "<INSERT-YOUR-API-KEY>"
+
+  func testSetup() {
+    let client = GenerativeLanguage(apiKey: apiKey)
     
-    func testSetup() {
-        let client = GenerativeLanguage(apiKey: apiKey)
-        
-        XCTAssertNotNil(client)
-        XCTAssertEqual(client.apiKey, apiKey)
-    }
-    
-    func testGenerateMessage() async throws {
-        let client = GenerativeLanguage(apiKey: apiKey)
-        let model = "models/chat-bison-001"
-        
-        let result = try await client.generateText(with: "Say something nice", model: model)
-        print(result)
-    }
-    
-    func testListModels() async throws {
-        let client = GenerativeLanguage(apiKey: apiKey)
-        
-        let result = try await client.listModels()
-        print(result.models ?? [])
-    }
-    
-    func testGetModel() async throws {
-        let client = GenerativeLanguage(apiKey: apiKey)
-        let model = "chat-bison-001"
-        
-        let result = try await client.getModel(name: model)
-        print(result.displayName ?? [])
-    }
+    XCTAssertNotNil(client)
+    XCTAssertEqual(client.apiKey, apiKey)
+  }
+
+  func testGenerateMessage() async throws {
+    let client = GenerativeLanguage(apiKey: apiKey)
+    let model = "models/chat-bison-001"
+
+    let result = try await client.generateText(with: "Say something nice", model: model)
+    print(result)
+  }
+
+  func testListModels() async throws {
+    let client = GenerativeLanguage(apiKey: apiKey)
+
+    let result = try await client.listModels()
+    print(result.models ?? [])
+  }
+
+  func testGetModel() async throws {
+    let client = GenerativeLanguage(apiKey: apiKey)
+    let model = "chat-bison-001"
+
+    let result = try await client.getModel(name: model)
+    print(result.displayName ?? [])
+  }
 }
