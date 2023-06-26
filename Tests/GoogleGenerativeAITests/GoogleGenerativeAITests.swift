f5f5eacf1fb3703fa99363ec79e4e513e7d6ebdb
Eric Jeon <jsharp83@gmail.com>
2023-06-26 21:55:17 +0900

    fix indentation

--- Tests/GoogleGenerativeAITests/GoogleGenerativeAITests.swift
+++ Tests/GoogleGenerativeAITests/GoogleGenerativeAITests.swift
@@ -33,44 +33,44 @@ final class GenerativeLanguageTests: XCTestCase {
       return value
     }
   }
-  
+
   override func setUp() {
     super.setUp()
     client = GenerativeLanguage(apiKey: apiKey)
-    
+
     XCTAssertNotNil(client)
     XCTAssertEqual(client.apiKey, apiKey)
   }
-  
+
   func testGenerateText() async throws {
     let model = "models/text-bison-001"
-    
+
     let result = try await client.generateText(with: "Say something nice", model: model)
     print(result)
   }
-  
+
   func testChat() async throws {
     let model = "models/chat-bison-001"
-    
+
     let result = try await client.chat(message: "Say something nice", model: model)
     print(result)
   }
-  
+
   func testGenerateEmbeddings() async throws {
     let model = "models/embedding-gecko-001"
-    
+
     let result = try await client.generateEmbeddings(from: "Say something nice", model: model)
     print(result)
   }
-  
+
   func testListModels() async throws {
     let result = try await client.listModels()
     print(result.models ?? [])
   }
-  
+
   func testGetModel() async throws {
     let model = "chat-bison-001"
-    
+
     let result = try await client.getModel(name: model)
     print(result.displayName ?? "")
   }
