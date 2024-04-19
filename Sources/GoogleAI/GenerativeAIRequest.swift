8e3787f48a8f47575f7a1afcac8bdef3c85e86ae
Andrew Heard <andrewheard@google.com>
2024-04-19 12:10:14 -0400

    Update default API version from `v1` to `v1beta` (#137)

--- Sources/GoogleAI/GenerativeAIRequest.swift
+++ Sources/GoogleAI/GenerativeAIRequest.swift
@@ -38,8 +38,8 @@ public struct RequestOptions {
   /// - Parameters:
   ///   - timeout The request’s timeout interval in seconds; if not specified uses the default value
   ///   for a `URLRequest`.
-  ///   - apiVersion The API version to use in requests to the backend; defaults to "v1".
-  public init(timeout: TimeInterval? = nil, apiVersion: String = "v1") {
+  ///   - apiVersion The API version to use in requests to the backend; defaults to "v1beta".
+  public init(timeout: TimeInterval? = nil, apiVersion: String = "v1beta") {
     self.timeout = timeout
     self.apiVersion = apiVersion
   }
