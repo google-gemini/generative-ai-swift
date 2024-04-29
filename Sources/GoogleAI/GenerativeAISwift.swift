82771de98819e50567022d9047fc6ae4a10f1b6d
Andrew Heard <andrewheard@google.com>
2024-04-29 17:44:39 -0400

    Increment SDK version to `0.5.2`

--- Sources/GoogleAI/GenerativeAISwift.swift
+++ Sources/GoogleAI/GenerativeAISwift.swift
@@ -21,6 +21,6 @@ import Foundation
 @available(iOS 15.0, macOS 11.0, macCatalyst 15.0, *)
 public enum GenerativeAISwift {
   /// String value of the SDK version
-  public static let version = "0.5.1"
+  public static let version = "0.5.2"
   static let baseURL = "https://generativelanguage.googleapis.com"
 }
